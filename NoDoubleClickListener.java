681031d8bbf7e8d84649d9d6e10ad321c97ebd22
Zephyr90 <zephyr.gzf@gmail.com>
2016-11-22 15:13:51 +0800

    防止多次点击的监听类

---
+++ NoDoubleClickListener.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2016/11/22 0022.
+ */
+public abstract class NoDoubleClickListener implements View.OnClickListener {
+
+    public static final int MIN_CLICK_DELAY_TIME = 1000;
+    private long lastClickTime = 0;
+
+    @Override
+    public void onClick(View v) {
+        long currentTime = Calendar.getInstance().getTimeInMillis();
+        if (currentTime - lastClickTime > MIN_CLICK_DELAY_TIME) {
+            lastClickTime = currentTime;
+            onNoDoubleClick(v);
+        }
+    }
+
+    protected abstract void onNoDoubleClick(View v);
+}
