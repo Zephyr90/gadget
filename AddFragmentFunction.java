0d000578f2b6646b8f5599d8492f843ac55f10b7
Zephyr90 <zephyr.gzf@gmail.com>
2016-11-29 17:22:11 +0800

    Android6.0权限请求，可以放到BaseActivity当中

---
+++ AddFragmentFunction.java
@@ -0,0 +1,21 @@
+    /*这个java文件不是完整的java类，只是为了记录以下添加framgent的方法*/
+    protected void addFragment(int frameLayoutId, Fragment fragment) {
+        if (fragment != null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            if (fragment.isAdded()) {
+                if (mFragment != null) {
+                    transaction.hide(mFragment).show(fragment);
+                } else {
+                    transaction.show(fragment);
+                }
+            } else {
+                if (mFragment != null) {
+                    transaction.hide(mFragment).add(frameLayoutId, fragment);
+                } else {
+                    transaction.add(frameLayoutId, fragment);
+                }
+            }
+            mFragment = fragment;
+            transaction.commit();
+        }
+    }
